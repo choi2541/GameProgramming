48d98ef18878e59e972f03dac47595b6ed87fc00
choi2541 <suhwan821@gmail.com>
2025-02-14 17:31:35 +0900

    (2025.02.14) Hello World 출력

---
+++ C/Cstudy/FileName.cpp
@@ -0,0 +1,19 @@
+// 주석- 컴퓨터는 알아볼 수 없다.
+/* 
+작성일자 : 2025-02-14
+작성자 : 최수환
+주제 : Hello World 출력하기
+*/
+//
+ 
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    return 0;
+}
+//
+
+
+
