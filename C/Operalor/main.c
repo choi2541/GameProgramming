04b23af82a8551089af1a6048d1f83750ea9669c
choi2541 <suhwan821@gmail.com>
2025-02-20 17:48:45 +0900

    (2025-02-20)데이터

--- C/Operalor/main.c
+++ C/Operalor/main.c
@@ -102,6 +102,18 @@ int main()
 	// 게임 스토리 1
 	//무기 강화할때
 	//n강일 때의 무기, n+1 무기의 정보화면에 출력하는 스토리를 작성해보세요
+	
+	int basePower = 10;
+	int weaponLv = 1;
+	int weight = 2;
+
+	int attackPower = basePower + weaponLv * weight;
+
+	printf("무기레벨 : %d, 무기의 공격력 : %d \n", weaponLv++, attackPower);
+	attackPower = basePower + weaponLv * weight;
+	printf("무기레벨 : %d, 무기의 공격력 : %d \n", weaponLv, attackPower);
+
+
 	//게임스토리 2
 
 	//아이템 인벤토리. Slot. 기타 아이템 수집. Stack x 갯수
@@ -110,7 +122,26 @@ int main()
 	// A 아이템을 n개 획득, B 아이템 m개 획득
 	// 현재 인벤토리의 갯수가 가득 찼는지 안찼는지 표현하는 결과를 표현해보세요.
 
+	int maxInventorySlot = 10;
+	int AStackCount = 200;
+	int BStackCount = 200;
+	int ADrop = 555;
+	int BDrop = 555;
+	printf("1시간 동안 사냥을 해서 아이템을 획득을 했다. \n");
+	printf("A아이템 : %d, B아이템 : %d 획득했다.\n", ADrop, BDrop);
+
+	// 나머지
+
+	int AInventoryCount = (ADrop % AStackCount) == 0 ? (ADrop / AStackCount) : (ADrop / AStackCount) + 1;
+	int BInventoryCount = (BDrop % BStackCount) == 0 ? (BDrop / BStackCount) : (BDrop / BStackCount) + 1;
+
+	printf(" A의 갯수 :%d B의 갯수 : %d\n", AInventoryCount, BInventoryCount);
+	printf("결과 :  %s\n", maxInventorySlot <= (AInventoryCount + BInventoryCount) ? "가득참" : "가득안참");
+
+
 	//3항 연산자.
+
+
 
 	//게임 스토리3. 작성해보세요.
 
